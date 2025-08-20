64da3a2cb18000dfc639a39a585e6f9e0912bb90
임지수 <dlawltn0708@naver.com>
2025-08-20 15:58:42 +0900

    feat: progressbar 컴포넌트 구현

---
+++ src/pages/data/voice/page/VoiceCollector.tsx
@@ -0,0 +1,11 @@
+import ProgressBar from '@/common/components/progress/ProgressBar';
+
+const VoiceCollector = () => {
+  return (
+    <div>
+      <ProgressBar total={100} current={24} />
+    </div>
+  );
+};
+
+export default VoiceCollector;
