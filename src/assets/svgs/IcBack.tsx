5c0c6c9e5fa9d95202ffcd79dd6e719d38ba13e3
임지수 <dlawltn0708@naver.com>
2025-08-20 13:43:05 +0900

    feat: svgr 설정 추가

--- src/assets/svgs/IcBack.tsx
+++ src/assets/svgs/IcBack.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgIcBack = (props: SVGProps<SVGSVGElement>) => (
   <svg
--- src/assets/svgs/IcProfile.tsx
+++ src/assets/svgs/IcProfile.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgIcProfile = (props: SVGProps<SVGSVGElement>) => (
   <svg
