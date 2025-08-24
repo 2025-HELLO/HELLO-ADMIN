adedc98b7550f54f8f07d3794b3cad5ad3336034
yangsy02 <eureka7655@naver.com>
2025-08-24 17:02:37 +0900

    feat: 홈 화면 카드 버튼 라우팅 추가 및 ChatCollector 페이지 라우트 연결

---
+++ src/pages/data/chat/page/ChatCollector.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ChatCollector: React.FC = () => {
+  return (
+    <div style={{ padding: '16px' }}>
+      <h1 style={{ fontSize: 18, fontWeight: 700, marginBottom: 12 }}>카카오톡 채팅 내역 제출</h1>
+      <p style={{ color: '#666' }}>
+        추후 이 화면에서 카카오톡 대화 내역 파일 업로드/검증/전송 흐름을 구현합니다.
+      </p>
+    </div>
+  );
+};
+
+export default ChatCollector;
