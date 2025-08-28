29aa5eb02cb09ced50d2d27e212404dd6bd0a831
임지수 <dlawltn0708@naver.com>
2025-08-28 18:17:33 +0900

    fix: 에러 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
-import { OverlayProvider } from 'overlay-kit';
 
 import { router } from './shared/routes/router';
 import { queryClient } from './common/utils/queryClient';
@@ -11,12 +10,10 @@ import * as styles from '@/shared/styles/global.css.ts';
 function App() {
   return (
     <div className={styles.rootContainer}>
-      <OverlayProvider>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-          {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
-        </QueryClientProvider>
-      </OverlayProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+      </QueryClientProvider>
     </div>
   );
 }
