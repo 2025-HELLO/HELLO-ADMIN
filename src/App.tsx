423b733aa7986e39bddc81a5e7a9f20eb01a72c5
임지수 <dlawltn0708@naver.com>
2025-08-28 18:15:07 +0900

    fix: 래빗리뷰 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
+import { OverlayProvider } from 'overlay-kit';
 
 import { router } from './shared/routes/router';
 import { queryClient } from './common/utils/queryClient';
@@ -10,10 +11,12 @@ import * as styles from '@/shared/styles/global.css.ts';
 function App() {
   return (
     <div className={styles.rootContainer}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
-      </QueryClientProvider>
+      <OverlayProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+        </QueryClientProvider>
+      </OverlayProvider>
     </div>
   );
 }
